8147c4e2bf937a2db68728ead4e8900798ab0019
muhamadijlal <haidarijlal027@gmail.com>
2022-07-04 06:47:34 +0700

    responsive herosection sm md lg

--- resources/views/menu/main.blade.php
+++ resources/views/menu/main.blade.php
@@ -1,13 +1,14 @@
 @extends('main.master')
 @section('content')
-<section class="hero mt-20 font-inter">  
-  <div class="title lg:w-3/4 lg:px-7 md:w-2/3 md:mx-5">
-    <p class="lg:text-8xl text-slate-800 font-bold block mb-4 md:text-7xl">Guide<span class="text-blue-500">Krw.</span></p>
-    <span class="subtitle font-semibold lg:text-4xl text-slate-700 block mb-3 md:text-3xl">Selamat Datang</span>
-    <p class="text-justify lg:text-lg md:text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut quaerat minus enim optio odit perspiciatis error nostrum natus et dolores, tenetur fuga? Tempora, nobis rerum? Blanditiis nulla quam itaque nisi impedit nostrum, ut maiores odit consectetur aperiam quibusdam, reiciendis iure quisquam, possimus pariatur similique sequi!</p>
-    <a href="#" class=" mt-10 bg-blue-500 lg:px-5 lg:py-2 lg:text-lg inline-block text-white rounded-full md:px-4 md:py-2 md:text-sm">Lorem, ipsum dolor.</a>
-  </div>
-  <div class="image">
+<section class="hero my-20 font-inter px-6 md:p-0 mx-auto">  
+  <div class="title md:w-4/5 md:mx-5">
+    <p class="text-slate-800 font-bold block mb-8 text-6xl sm:text-8xl md:text-5xl lg:text-8xl">Guide<span class="text-blue-500">Krw.</span></p>
+    <span class="subtitle text-2xl font-semibold text-slate-700 block mb-1 sm:text-4xl md:text-2xl lg:text-4xl">Selamat Datang</span>
+    <p class="text-justify text-base sm:text-lg md:text-base lg:text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut quaerat minus enim optio odit perspiciatis error nostrum natus et dolores, tenetur fuga? Tempora, nobis rerum? Blanditiis nulla quam itaque nisi impedit nostrum, ut maiores odit consectetur aperiam quibusdam, reiciendis iure quisquam, possimus pariatur similique sequi!</p>
+    <a href="#" class=" mt-5 mb-14 bg-blue-500 inline-block text-white text-sm py-2 px-3 sm:text-md sm:py-3 sm:px-5 md:text-sm  md:py-2 md:px-3 rounded-full lg:py-2 lg:px-3 lg:text-lg">Lorem, ipsum dolor.</a>
+    <img src="{{ asset('assets/img/tugu-padi.jpg') }}" alt="tugu padi karawang" class="rounded-l-full md:hidden lg:hidden">
+  </div>  
+  <div class="image hidden md:block">
     <img src="{{ asset('assets/img/tugu-padi.jpg') }}" alt="tugu padi karawang" class="rounded-l-full">
   </div>
 </section>
